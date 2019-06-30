3eab662d87fc73d3bec02b120644ca0118c794bf
Madeline Lemberg <mvlemberg@gmail.com>
2019-06-30 03:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ function logShout(string)
 {
   console.log(string.toUpperCase());
 }
+function logWhisper(string)
+{
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string)
+{
+  if 
+}
+}
